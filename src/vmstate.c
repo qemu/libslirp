qemu/libslirp

commit 6155c349156b3fd52434880dc73cedfd4e8395a9

vmstate: pass on read/write errors for state

src/vmstate.c
@@ -240,15 +240,13 @@ const VMStateInfo slirp_vmstate_info_tmp = {
 static int get_buffer(SlirpIStream *f, void *pv, size_t size,
                       const VMStateField *field)
 {
-    slirp_istream_read(f, pv, size);
-    return 0;
+    return slirp_istream_read(f, pv, size);
 }
 
 static int put_buffer(SlirpOStream *f, void *pv, size_t size,
                       const VMStateField *field)
 {
-    slirp_ostream_write(f, pv, size);
-    return 0;
+    return slirp_ostream_write(f, pv, size);
 }
 
 const VMStateInfo slirp_vmstate_info_buffer = {

commit cb2755b3134650f380c3fbb697f507a6b53bad26

slirp: Fix migration break on incorrect vmstate retcode
Commit 6155c34 while cleaning up the code provided an inverted retval for
the vmstate get()/put() handlers.  Fix it.

Quite a few QEMU CI tests are affected by this problem.  One reproducer:

  (under qemu build dir)
  $ QTEST_QEMU_BINARY=./qemu-system-x86_64 ./tests/qtest/migration-test

Closes #94.

Fixes: 6155c34 ("vmstate: pass on read/write errors for state")
Signed-off-by: Peter Xu <peterx@redhat.com>

src/vmstate.c
@@ -240,13 +240,13 @@ const VMStateInfo slirp_vmstate_info_tmp = {
 static int get_buffer(SlirpIStream *f, void *pv, size_t size,
                       const VMStateField *field)
 {
-    return slirp_istream_read(f, pv, size);
+    return !slirp_istream_read(f, pv, size);
 }
 
 static int put_buffer(SlirpOStream *f, void *pv, size_t size,
                       const VMStateField *field)
 {
-    return slirp_ostream_write(f, pv, size);
+    return !slirp_ostream_write(f, pv, size);
 }
 
 const VMStateInfo slirp_vmstate_info_buffer = {